80f0c899a07cf230f92029f8ca233de7ad085056
Tom Meadon <tom.meadon@acosta-europe.com>
2019-10-09 14:20:39 +0100

    Added custom exception

---
+++ src/PSServiceBus/Exceptions/NonExistentEntityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSServiceBus.Exceptions
+{
+    public class NonExistentEntityException : Exception
+    {
+        public NonExistentEntityException() : base() { }
+
+        public NonExistentEntityException(string message) : base(message) { }
+    }
+}
