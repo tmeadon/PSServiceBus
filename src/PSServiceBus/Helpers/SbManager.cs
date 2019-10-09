394ef326b494e0ad2b3c4bc00dd502838221b1c3
Tom Meadon <tom.meadon@acosta-europe.com>
2019-10-09 14:20:18 +0100

    Added error handling

--- src/PSServiceBus/Helpers/SbManager.cs
+++ src/PSServiceBus/Helpers/SbManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Azure.ServiceBus.Management;
+using PSServiceBus.Exceptions;
 
 namespace PSServiceBus.Helpers
 {
@@ -16,7 +17,14 @@ namespace PSServiceBus.Helpers
 
         public QueueDescription GetQueueByName(string QueueName)
         {
-            return managementClient.GetQueueAsync(QueueName).Result;
+            try
+            {
+                return managementClient.GetQueueAsync(QueueName).Result;
+            }
+            catch
+            {
+                throw new NonExistentEntityException(String.Format("Queue {0} does not exist", QueueName));
+            }
         }
 
         public IList<QueueDescription> GetAllQueues()
@@ -26,7 +34,14 @@ namespace PSServiceBus.Helpers
 
         public TopicDescription GetTopicByName(string TopicName)
         {
-            return managementClient.GetTopicAsync(TopicName).Result;
+            try
+            {
+                return managementClient.GetTopicAsync(TopicName).Result;
+            }
+            catch
+            {
+                throw new NonExistentEntityException(String.Format("Topic {0} does not exist", TopicName));
+            }
         }
 
         public IList<TopicDescription> GetAllTopics()
@@ -36,7 +51,16 @@ namespace PSServiceBus.Helpers
 
         public SubscriptionDescription GetSubscriptionByName(string TopicName, string SubscriptionName)
         {
-            return managementClient.GetSubscriptionAsync(TopicName, SubscriptionName).Result;
+            var topic = this.GetTopicByName(TopicName);
+
+            try
+            {
+                return managementClient.GetSubscriptionAsync(TopicName, SubscriptionName).Result;
+            }
+            catch
+            {
+                throw new NonExistentEntityException(String.Format("Subscription {0} does not exist in topic {1}", SubscriptionName, TopicName));
+            }
         }
 
         public IList<SubscriptionDescription> GetAllSubscriptions(string TopicName)
@@ -46,11 +70,15 @@ namespace PSServiceBus.Helpers
 
         public QueueRuntimeInfo GetQueueRuntimeInfo(string QueueName)
         {
-            return managementClient.GetQueueRuntimeInfoAsync(QueueName).Result;
+            var queue = this.GetQueueByName(QueueName);
+
+            return managementClient.GetQueueRuntimeInfoAsync(QueueName).Result; 
         }
 
         public SubscriptionRuntimeInfo GetSubscriptionRuntimeInfo(string TopicName, string SubscriptionName)
         {
+            var sub = this.GetSubscriptionByName(TopicName, SubscriptionName);
+
             return managementClient.GetSubscriptionRuntimeInfoAsync(TopicName, SubscriptionName).Result;
         }
     }
