61dd6ee52adc2501b3875c26ac3243a117361fa7
Tom Meadon <tom.meadon@acosta-europe.com>
2019-10-10 15:01:25 +0100

    Add new output type for messages

---
+++ src/PSServiceBus/Outputs/SbMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSServiceBus.Outputs
+{
+    public class SbMessage
+    {
+        public string MessageId;
+
+        public string MessageBody;
+    }
+}
