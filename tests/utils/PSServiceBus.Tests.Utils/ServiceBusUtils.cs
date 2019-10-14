093df037eaf3f5ff8a2802a3c7a4c5d837c34a1b
tommeadon <tom@meadon.me>
2019-10-14 21:47:19 +0100

    Save connection string

--- tests/utils/PSServiceBus.Tests.Utils/ServiceBusUtils.cs
+++ tests/utils/PSServiceBus.Tests.Utils/ServiceBusUtils.cs
@@ -5,19 +5,22 @@ namespace PSServiceBus.Tests.Utils
 {
     public class ServiceBusUtils
     {
+        public string NamespaceConnectionString;
+
         private readonly ManagementClient managementClient;
 
         public ServiceBusUtils (string NamespaceConnectionString)
         {
+            this.NamespaceConnectionString = NamespaceConnectionString;
             this.managementClient = new ManagementClient(NamespaceConnectionString);
         }
 
-        public void CreateQueue (string QueueName)
+        public void CreateQueue(string QueueName)
         {
             this.managementClient.CreateQueueAsync(QueueName);
         }
 
-        public void RemoveQueue (string QueueName)
+        public void RemoveQueue(string QueueName)
         {
             this.managementClient.DeleteQueueAsync(QueueName);
         }
