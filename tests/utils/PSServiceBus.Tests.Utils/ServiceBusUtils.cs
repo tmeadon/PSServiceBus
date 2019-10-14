42e54f0cef60f5754cd9fa83167776c75fb92d28
tommeadon <tom@meadon.me>
2019-10-14 18:09:36 +0100

    Add starter test util project

---
+++ tests/utils/PSServiceBus.Tests.Utils/ServiceBusUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace PSServiceBus.Tests.Utils
+{
+    public class ServiceBusUtils
+    {
+        private readonly ManagementClient managementClient;
+
+        public ServiceBusUtils (string NamespaceConnectionString)
+        {
+            this.managementClient = new ManagementClient(NamespaceConnectionString);
+        }
+
+        public void CreateQueue (string QueueName)
+        {
+            this.managementClient.CreateQueueAsync(QueueName);
+        }
+
+        public void RemoveQueue (string QueueName)
+        {
+            this.managementClient.DeleteQueueAsync(QueueName);
+        }
+    }
+}
