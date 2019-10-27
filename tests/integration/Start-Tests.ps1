a2cf21ae9b9ddafd38998b2aa7d4e1492d9dc4ed
tommeadon <tom@meadon.me>
2019-10-27 11:15:51 +0000

    Refactored test runner script to use new helper functions

--- tests/integration/Start-Tests.ps1
+++ tests/integration/Start-Tests.ps1
@@ -1,18 +1,35 @@
 [CmdletBinding()]
-param (
-    [Parameter()]
-    [string]
-    $TestServiceBusConnectionString
-)
+Param ()
 
+# load required modules and functions
 Import-Module -Name Pester -RequiredVersion 4.9.0
+Import-Module -Name Az.Resources
+Import-Module -Name Az.ServiceBus
 Import-Module $PSScriptRoot\..\..\output\PSServiceBus\PSServiceBus.psd1
 Import-Module $PSScriptRoot\..\utils\PSServiceBus.Tests.Utils\bin\Release\netstandard2.0\PSServiceBus.Tests.Utils.dll
-$sbUtils = [PSServiceBus.Tests.Utils.ServiceBusUtils]::new($TestServiceBusConnectionString)
+. $PSScriptRoot\..\utils\functions\IntegrationTestsFunctions.ps1
 
-Invoke-Pester -Script @{
-    Path = "$PSScriptRoot\GetSbQueue.tests.ps1"
-    Parameters = @{
-        ServiceBusUtils = $sbUtils
+# prepare for test run
+Write-Verbose -Message 'Preparing test run'
+
+$testEnvironment = Initialize-IntegrationTestRun -Location 'uk south'
+
+Write-Verbose -Message "Created environment $( $testEnvironment | ConvertTo-Json -Compress )"
+
+$sbUtils = [PSServiceBus.Tests.Utils.ServiceBusUtils]::new($testEnvironment.ConnectionString)
+
+# run tests
+foreach ($file in (Get-ChildItem -Path $PSScriptRoot -Filter '*.tests.ps1'))
+{
+    Invoke-Pester -Script @{
+        Path = $file.FullName
+        Parameters = @{
+            ServiceBusUtils = $sbUtils
+        }
     }
 }
+
+# tear down environment
+Write-Verbose -Message 'Removing test environment'
+
+Complete-IntegrationTestRun -ResourceGroupName $testEnvironment.ResourceGroupName -Confirm:$false
