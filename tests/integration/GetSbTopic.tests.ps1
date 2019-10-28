0425981b00740e90d429c7366de93fe0cd5f12ef
tommeadon <tom@meadon.me>
2019-10-28 17:50:21 +0000

    Completed integration tests for get-sbtopic

--- tests/integration/GetSbTopic.tests.ps1
+++ tests/integration/GetSbTopic.tests.ps1
@@ -37,6 +37,13 @@ Describe "Get-SbTopic tests" {
             $result.count | Should -EQ $topics.count
         }
 
+        It "should return the correct number of subscriptions in each topic" {
+            foreach ($item in $result)
+            {
+                $item.Subscriptions.count | Should -Be $topics.Where({$_.Topic -eq $item.TopicName}).Subscriptions.count
+            }
+        }
+
         It "should return the correct subscriptions in each topic" {
             foreach ($item in $result)
             {
@@ -44,6 +51,39 @@ Describe "Get-SbTopic tests" {
                 {
                     $item.Subscriptions[$i] | Should -BeIn $topics.Where({$_.Topic -eq $item.TopicName}).Subscriptions
                 } 
+            }
+        }
+    }
+
+    Context "Test with -TopicName parameter" {
+
+        $testCases = @(
+            @{
+                topic = $topics[0]
+            },
+            @{
+                topic = $topics[1]
+            }
+        )
+
+        It "should return the correct topic" -TestCases $testCases {
+            param ([PSCustomObject] $topic)
+            $result = Get-SbTopic -NamespaceConnectionString $ServiceBusUtils.NamespaceConnectionString -TopicName $topic.Topic
+            $result.TopicName | Should -Be $topic.Topic
+        }
+
+        It "should return the correct number of subscriptions in a topic" -TestCases $testCases {
+            param ([PSCustomObject] $topic)
+            $result = Get-SbTopic -NamespaceConnectionString $ServiceBusUtils.NamespaceConnectionString -TopicName $topic.Topic
+            $result.Subscriptions.count | Should -Be $topic.Subscriptions.count   
+        }
+
+        It "should return the correct subscriptions in a topic" -TestCases $testCases {
+            param ([PSCustomObject] $topic)
+            $result = Get-SbTopic -NamespaceConnectionString $ServiceBusUtils.NamespaceConnectionString -TopicName $topic.Topic
+            for ($i = 0; $i -lt $subscriptionsToCreatePerTopic; $i++)
+            {
+                $result.Subscriptions[$i] | Should -BeIn $topic.Subscriptions
             }
         }
     }
