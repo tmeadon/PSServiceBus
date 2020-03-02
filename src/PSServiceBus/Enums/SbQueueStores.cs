3ef63d61b05d3f687c9e8d02834ab9e3bb031b20
tommeadon <tom@meadon.me>
2020-03-02 11:31:00 +0000

    add new enum for queue stores

---
+++ src/PSServiceBus/Enums/SbQueueStores.cs
@@ -0,0 +1,23 @@
+﻿namespace PSServiceBus.Enums
+{
+    /// <summary>
+    /// Valid Service Bus queue stores
+    /// </summary>
+    public enum SbQueueStores
+    {
+        /// <summary>
+        /// The 'active' queue store
+        /// </summary>
+        Active,
+
+        /// <summary>
+        /// The 'deadletter' queue store
+        /// </summary>
+        DeadLetter,
+
+        /// <summary>
+        /// The 'scheduled' queue store
+        /// </summary>
+        Scheduled
+    }
+}
